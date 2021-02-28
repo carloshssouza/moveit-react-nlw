d1a9c3243afd7b2661ba91083c4c9270c9dc4502
Carlos <carlossouza_94@hotmail.com>
2021-02-28 14:43:10 -0300

    Projeto Finalizado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,32 +8,57 @@ import { ChallengeBox } from '../components/ChallengeBox';
 
 import styles from '../styles/pages/Home.module.css';
 import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
+
+interface HomeProps{
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
 
 
-export default function Home() {
+export default function Home(props) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Inicio | move.it</title>
-      </Head>
+    <ChallengesProvider 
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted} 
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Inicio | move.it</title>
+        </Head>
 
-      <ExperienceBar/>
+        <ExperienceBar/>
 
-      <CountdownProvider>
-        <section>
-          <div>
-            <Profile />
-            <CompletedChallenges />
-            <Countdown />
-          </div>
-          
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-      
-    </div>
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
       
   )
+}
+
+export const getServerSideProps = async(ctx) =>{
+
+  const{ level, currentExperience, challengesCompleted } = ctx.req.cookies;
+
+  return{
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  }
 }
